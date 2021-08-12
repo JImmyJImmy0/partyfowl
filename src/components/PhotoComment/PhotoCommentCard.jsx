d84faadeb3db566077123a678863604466995136
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-12 13:53:03 -0600

    make folder structer and stub photo card

---
+++ src/components/PhotoComment/PhotoCommentCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// components
+    // photo comments actions
+
+const PhotoCommentCard = (props) => {
+
+    return (
+        <div className='pComment-card'>
+
+            <div className='pCard-header'>
+
+            </div>
+
+            <div className='pComment-content'>
+                <p>
+
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default PhotoCommentCard
