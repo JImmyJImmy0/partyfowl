fd455488058563692bd394e31ea07a1369d647e2
JImmyJImmy0 <jerickson317@gmail.com>
2021-08-12 13:58:24 -0600

    stub p comment list

---
+++ src/components/PhotoComment/PhotoCommentList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// component
+    // comment card
+
+const PhotoCommentList = (props) => {
+
+    return (
+        <p></p>
+        // props.photoCommentsArray.map((photoComment) => (
+            // <PhotoCommentCard
+                // key={photoComment._id}
+                // photoComment={photoComment}
+                // {...props}
+            // />
+        // ))
+    )
+}
+
+export default PhotoCommentList
